e28712a61509d82dfc9d20d2fad1af4f1e70a591
deblnia <deblina@uchicago.edugit config --global user.name Deblina>
2024-04-16 14:42:15 -0400

    saving, but haven't finished much

--- ch1/1.1.rkt
+++ ch1/1.1.rkt
@@ -2,54 +2,75 @@
 
 #| 1.1 |#
 
-; 10 ;10 
-; (+ 5 3 4) ;12
-; (- 9 1) ;8 
-; (/ 6 2) ;3 
-; (+ (* 2 4) (- 4 6)) ;10 wrong, subtract right from left! 
-; (define a 3)
-; (define b (+ a 1)) 
-; (+ a b (* a b)) ; 19 
-; (= a b) ; false 
-; (if (and (> b a) (< b (* a b)))
-;     b
-;     a) ; 3 wrong,  read as operand operator operand 
-; (cond ((= a 4) 6)
-;       ((= b 4) (+ 6 7 a))
-;       (else 25)) ; 16
-; (+ 2 (if (> b a) b a)) ; 9, wrong don't know why
-; (* (cond ((> a b) a)
-;          ((< a b) b)
-;          (else -1))
-;    (+ a 1)) ; 16
+10 ;10 
+(+ 5 3 4) ;12
+(- 9 1) ;8 
+(/ 6 2) ;3 
+(+ (* 2 4) (- 4 6)) ;10 wrong, subtract right from left! 
+(define a 3)
+(define b (+ a 1)) 
+(+ a b (* a b)) ; 19 
+(= a b) ; false 
+(if (and (> b a) (< b (* a b)))
+    b
+    a) ; 3 wrong,  read as operand operator operand 
+(cond ((= a 4) 6)
+      ((= b 4) (+ 6 7 a))
+      (else 25)) ; 16
+(+ 2 (if (> b a) b a)) ; 9, wrong don't know why
+(* (cond ((> a b) a)
+         ((< a b) b)
+         (else -1))
+   (+ a 1)) ; 16
 
 
 #| 1.2 |#
 
-; (/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5))))) (* 3 (- 6 2) (- 2 7)))
+(/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5))))) (* 3 (- 6 2) (- 2 7)))
 ; how do i pretty print 
 
 
 #| 1.3 |#
 
-; (define (square x) (* x x))
+(define (square x) (* x x))
 
-; (define (max a b) (if (> a b) a b))
+(define (max a b) (if (> a b) a b))
 
-; (define (min a b) (if (< a b) a b))
+(define (min a b) (if (< a b) a b))
 
-; (define (sum-of-squares-two-largest a b c)
-;    (+ (square (max a b)) (square (max c (min a b)))))
+(define (sum-of-squares-two-largest a b c)
+   (+ (square (max a b)) (square (max c (min a b)))))
 
-; (sum-of-squares-two-largest 1 2 3)
-; (sum-of-squares-two-largest 1 2 1)
+(sum-of-squares-two-largest 1 2 3)
+(sum-of-squares-two-largest 1 2 1)
 ; borrowed from https://stackoverflow.com/questions/54165593/define-a-procedure-that-takes-three-numbers-as-arguments-and-returns-the-sum-of 
 ; sorrel and i might have paired on this one 
 
 #| 1.4 |#
 
-; a + b if b > 0, a - b if b <= 0 
+a + b if b > 0, a - b if b <= 0 
 (define (a-plus-abs-b a b)
   ((if (> b 0) + -) a b))
 
 (a-plus-abs-b 2 -4)
+
+
+#| 1.5 |#
+
+#| 
+    If the interpreter users applicative order evalution, Ben will see the interpreter evaluate the arguments in order and then apply. 
+
+    If the interpreter uses normal order evaluation, the interpreter will fully expand the then reduce (delaying evaluating operands until values are needed). 
+
+    More details: https://sicp-solutions.net/post/sicp-solution-exercise-1-5/
+
+|#
+
+#| 1.6 |#
+
+#| 
+    new-if is a function, so each parameter subexpression will be evaluated before the procedure is applied. 
+
+
+|#
+
